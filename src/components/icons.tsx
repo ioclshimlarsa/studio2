bdec12456e8b1d89dee1ea83251de94f09bd6b67
SRMP 2023 Shimla RSA <srmpshimlarsa@gmail.com>
2025-08-26 11:24:42 +0000

    change landing page text to heading Bharat Scouts and Guides CampConnect

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -6,7 +6,7 @@ export function Logo({ className, ...props }: SVGProps<SVGSVGElement>) {
   return (
     <div className={cn("flex items-center gap-2 text-primary", className)} {...props}>
       <Tent className="h-7 w-7" />
-      <span className="text-xl font-bold font-headline">CampConnect Punjab</span>
+      <span className="text-xl font-bold font-headline">Bharat Scouts and Guides CampConnect Punjab</span>
     </div>
   );
 }
